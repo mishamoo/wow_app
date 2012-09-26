1a69da83e82fe725a4ca2baf371b759a310478c4
Mijail Benitez <mijailbenitez@gmail.com>
2012-09-26 14:58:02 -0400

    initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WowApp::Application.config.secret_token = '9bb41d13269962cd3e8d20ed241c8cd8444d034bf303f22c5ea0e3139559fa84da466e9df3e70ec6a6a0b60adebfdba8a2d77b3dda8287b5326e0490d53bed86'
